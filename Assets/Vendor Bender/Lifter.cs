9927bb8b848e1f2a77b441a3d80ea90f6fad257f
brendan.poole <brendan.poole@gmail.com>
2014-11-08 18:58:31 +0000

    Make lifters work in sync

--- Assets/Vendor Bender/Lifter.cs
+++ Assets/Vendor Bender/Lifter.cs
@@ -4,25 +4,26 @@ using System.Collections;
 public class Lifter : MonoBehaviour {
 	float StartY;
 	public bool direction;
-	float frame = 0;
+
 	// Use this for initialization
 	void Start () {
+		rigidbody.position = new Vector3 (transform.parent.position.x, transform.parent.position.y + .5f, transform.parent.position.z);
 		StartY = rigidbody.position.y;
-		if (direction) {
-			frame += 180f;
-		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		float targetPosition = StartY + Mathf.Sin (frame * Mathf.Deg2Rad) * 1f;
+		float angle = Time.frameCount;
+		if (direction) {
+						angle += 180f;
+				}
+		float targetPosition = StartY + Mathf.Sin (angle * Mathf.Deg2Rad) * .5f;
 		float diff = rigidbody.position.y - targetPosition;
 		if (diff < 0) {
 						rigidbody.velocity =  (new Vector3 (0, 1, 0));
 				} else {
 			rigidbody.velocity =  (new Vector3 (0, -1, 0));
 				}
-		frame += 1f;
 	}
 }
